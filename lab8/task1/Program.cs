9923b4edd405678cb540e9d188dcfd9ab39288b3
Tnips <95481422+Tnips@users.noreply.github.com>
2024-04-26 15:46:15 +0300

    Complete task1-2

---
+++ lab8/task1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+	public struct Student
+	{
+		public string FullName;
+		public int GroupNumber;
+		public int[] Performance;
+	}
+
+	internal class Program
+	{
+		static void Main()
+		{
+			Student[] students = new Student[10];
+
+			for (int i = 0; i < 10; i++)
+			{
+				Console.WriteLine("Введите фамилию и инициалы студента:");
+				string fullName = Console.ReadLine();
+
+				Console.WriteLine("Введите номер группы:");
+				int groupNumber = int.Parse(Console.ReadLine());
+
+				Console.WriteLine("Введите оценки студента (5 чисел через пробел):");
+				int[] performance = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+				students[i] = new Student { FullName = fullName, GroupNumber = groupNumber, Performance = performance };
+			}
+
+			students = students.OrderBy(s => s.GroupNumber).ToArray();
+
+			bool found = false;
+			foreach (var student in students)
+			{
+				double average = student.Performance.Average();
+				if (average > 4.0)
+				{
+					Console.WriteLine($"Студент: {student.FullName}, Группа: {student.GroupNumber}");
+					found = true;
+				}
+			}
+
+			if (!found)
+			{
+				Console.WriteLine("Студентов со средним баллом больше 4.0 не найдено.");
+			}
+		}
+	}
+}
---
+++ lab8/task2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task2
+{
+	public enum Post
+	{
+		Director = 160,
+		Manager = 170,
+		Engineer = 180,
+		Clerk = 190, 
+		Intern = 150 
+	}
+
+	public class Accountant
+	{
+		public bool AskForBonus(Post worker, int hours)
+		{
+			if (hours > (int)worker)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+	}
+
+	internal class Program
+	{
+		static void Main()
+		{
+			Accountant accountant = new Accountant();
+
+			Post worker = Post.Engineer;
+			int hours = 190;
+
+			bool bonus = accountant.AskForBonus(worker, hours);
+
+			if (bonus)
+			{
+				Console.WriteLine("Премия выдается.");
+			}
+			else
+			{
+				Console.WriteLine("Премия не выдается.");
+			}
+			Console.ReadLine();
+		}
+	}
+
+}
