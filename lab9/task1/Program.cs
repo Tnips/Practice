69312c8bd599976c227b0a35a31fba8485193f0e
Tnips <95481422+Tnips@users.noreply.github.com>
2024-04-27 10:42:08 +0300

    Complete task1-2

---
+++ lab9/task1/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+	internal class Program
+	{
+		interface Ix
+		{
+			void IxF0(double w);
+			void IxF1();
+		}
+
+		interface Iy
+		{
+			void F0(double w);
+			void F1();
+		}
+
+		interface Iz
+		{
+			void F0(double w);
+			void F1();
+		}
+
+		class TestClass : Ix, Iy, Iz
+		{
+			double w;
+
+			public void IxF0(double w)
+			{
+				this.w = w;
+				Console.WriteLine(Math.Sqrt(w));
+			}
+
+			public void IxF1()
+			{
+				Console.WriteLine(Math.Sqrt(w));
+			}
+
+			public void F0(double w)
+			{
+				this.w = w;
+				Console.WriteLine(Math.Sqrt(w));
+			}
+
+			public void F1()
+			{
+				Console.WriteLine(Math.Sqrt(w));
+			}
+
+			void Iz.F0(double w)
+			{
+				this.w = w;
+				Console.WriteLine(w * w + 5);
+			}
+
+			void Iz.F1()
+			{
+				Console.WriteLine(w * w + 5);
+			}
+		}
+		static void Main(string[] args)
+		{
+			TestClass test = new TestClass();
+
+			test.IxF0(4.0);
+			test.IxF1();
+
+			test.F0(9.0);
+			test.F1();
+
+			((Iz)test).F0(16.0);
+			((Iz)test).F1();
+			Console.ReadLine();
+		}
+	}
+}
---
+++ lab9/task2/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task2
+{
+	public class Vector : IComparable<Vector>
+	{
+		private int[] array;
+
+		public Vector(int size)
+		{
+			array = new int[size];
+		}
+
+		public int this[int index]
+		{
+			get
+			{
+				if (index >= 0 && index < array.Length)
+					return array[index];
+				else
+					throw new IndexOutOfRangeException();
+			}
+			set
+			{
+				if (index >= 0 && index < array.Length)
+					array[index] = value;
+				else
+					throw new IndexOutOfRangeException();
+			}
+		}
+
+		public static Vector operator +(Vector a, Vector b)
+		{
+			if (a.array.Length != b.array.Length)
+				throw new ArgumentException("Vectors must be of the same size");
+
+			Vector result = new Vector(a.array.Length);
+			for (int i = 0; i < a.array.Length; i++)
+				result[i] = a[i] + b[i];
+
+			return result;
+		}
+
+		public static Vector operator -(Vector a, Vector b)
+		{
+			if (a.array.Length != b.array.Length)
+				throw new ArgumentException("Vectors must be of the same size");
+
+			Vector result = new Vector(a.array.Length);
+			for (int i = 0; i < a.array.Length; i++)
+				result[i] = a[i] - b[i];
+
+			return result;
+		}
+
+		public void Print()
+		{
+			foreach (var item in array)
+				Console.Write(item + " ");
+			Console.WriteLine();
+		}
+
+		public int CompareTo(Vector other)
+		{
+			if (other == null) return 1;
+
+			if (this.array.Length > other.array.Length)
+				return 1;
+			else if (this.array.Length < other.array.Length)
+				return -1;
+			else
+				return 0;
+		}
+	}
+
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Vector v1 = new Vector(5);
+			Vector v2 = new Vector(5);
+
+			for (int i = 0; i < 5; i++)
+			{
+				v1[i] = i;
+				v2[i] = i * 2;
+			}
+
+			Console.WriteLine("Вектор v1:");
+			v1.Print();
+
+			Console.WriteLine("Вектор v2:");
+			v2.Print();
+
+			Vector v3 = v1 + v2;
+			Console.WriteLine("v1 + v2:");
+			v3.Print();
+
+			Vector v4 = v1 - v2;
+			Console.WriteLine("v1 - v2:");
+			v4.Print();
+			Console.ReadLine();
+		}
+	}
+}
