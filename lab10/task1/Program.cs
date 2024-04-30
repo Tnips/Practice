17c7661589b451968df5a0fd3ed7cd484cd50000
Tnips <95481422+Tnips@users.noreply.github.com>
2024-04-30 13:33:42 +0300

    complete task1-2

---
+++ lab10/task1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+	public class Apartment
+	{
+		public string Name { get; set; }
+		public double CostPerSquareMeter { get; set; }
+		public double Area { get; set; }
+
+		public double TotalCost()
+		{
+			return CostPerSquareMeter * Area;
+		}
+	}
+
+	public class CentralApartment : Apartment
+	{
+		public string DistrictName { get; set; }
+
+		public new double TotalCost()
+		{
+			return base.TotalCost() * 1.01; 
+		}
+	}
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			CentralApartment centralApartment = new CentralApartment();
+
+			Console.Write("Введите название квартиры: ");
+			centralApartment.Name = Console.ReadLine();
+
+			Console.Write("Введите стоимость за квадратный метр: ");
+			centralApartment.CostPerSquareMeter = Convert.ToDouble(Console.ReadLine());
+
+			Console.Write("Введите площадь квартиры: ");
+			centralApartment.Area = Convert.ToDouble(Console.ReadLine());
+
+			Console.Write("Введите название района: ");
+			centralApartment.DistrictName = Console.ReadLine();
+
+			Console.WriteLine($"Общая стоимость квартиры {centralApartment.Name}, расположенной в районе {centralApartment.DistrictName}, составляет {centralApartment.TotalCost()}");
+			Console.ReadLine();
+		}
+	}
+}
---
+++ lab10/task2/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task2
+{
+	public class A
+	{
+		protected double a = 5;
+		protected double b = 10;
+		public double c
+		{
+			get
+			{
+				return a + b;
+			}
+		}
+	}
+	public class B : A
+	{
+		private double d = 15;
+		public double c2
+		{
+			get
+			{
+				if (d > 10)
+				{
+					return a + b + d;
+				}
+				else
+				{
+					return a + b;
+				}
+			}
+		}
+		public B() : base()
+		{
+		}
+		public B(double a, double b, double d)
+		{
+			this.a = a;
+			this.b = b;
+			this.d = d;
+		}
+	}
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			A a = new A();
+			Console.WriteLine($"Значение свойства c класса A: {a.c}");
+			B b1 = new B();
+			Console.WriteLine($"Значение свойства c2 класса B (с использованием конструктора, наследуемого от класса A): {b1.c2}");
+			B b2 = new B(7, 8, 9);
+			Console.WriteLine($"Значение свойства c2 класса B (с использованием собственного конструктора): {b2.c2}");
+			Console.ReadLine();
+		}
+	}
+}
