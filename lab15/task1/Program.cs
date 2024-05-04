357284409c6c39f2e44d2ad840be47952f65342c
Tnips <95481422+Tnips@users.noreply.github.com>
2024-05-04 11:30:50 +0300

    Complete task1-3

---
+++ lab15/task1/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+	public class Program
+	{
+		public static void Main()
+		{
+			MyList<int> myList = new MyList<int>();
+			myList.Add(1);
+			myList.Add(2);
+			myList.Add(3);
+
+			Console.WriteLine("Элементы в списке:");
+			for (int i = 0; i < myList.Count; i++)
+			{
+				Console.WriteLine(myList[i]);
+			}
+			Console.ReadLine();
+		}
+	}
+
+	public class MyList<T>
+	{
+		private T[] _items;
+		private int _count;
+
+		public MyList()
+		{
+			_items = new T[0];
+		}
+
+		public T this[int index]
+		{
+			get
+			{
+				if (index < 0 || index >= _count)
+				{
+					throw new IndexOutOfRangeException();
+				}
+				return _items[index];
+			}
+			set
+			{
+				if (index < 0 || index >= _count)
+				{
+					throw new IndexOutOfRangeException();
+				}
+				_items[index] = value;
+			}
+		}
+
+		public int Count
+		{
+			get { return _count; }
+		}
+
+		public void Add(T item)
+		{
+			if (_count == _items.Length)
+			{
+				int newCapacity = _count == 0 ? 4 : _count * 2;
+				T[] newItems = new T[newCapacity];
+				if (_count > 0)
+				{
+					Array.Copy(_items, 0, newItems, 0, _count);
+				}
+				_items = newItems;
+			}
+			_items[_count] = item;
+			_count++;
+		}
+	}
+}
---
+++ lab15/task2/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task2
+{
+	public class MyDictionary<TKey, TValue>
+	{
+		private List<TKey> keys;
+		private List<TValue> values;
+
+		public MyDictionary()
+		{
+			keys = new List<TKey>();
+			values = new List<TValue>();
+		}
+
+		public TValue this[TKey key]
+		{
+			get
+			{
+				int index = keys.IndexOf(key);
+				if (index == -1)
+				{
+					throw new ArgumentException("Key not found");
+				}
+				return values[index];
+			}
+		}
+
+		public void Add(TKey key, TValue value)
+		{
+			if (keys.Contains(key))
+			{
+				throw new ArgumentException("An element with the same key already exists");
+			}
+			keys.Add(key);
+			values.Add(value);
+		}
+
+		public int Count
+		{
+			get { return keys.Count; }
+		}
+	}
+
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			MyDictionary<string, int> myDictionary = new MyDictionary<string, int>();
+			myDictionary.Add("one", 1);
+			myDictionary.Add("two", 2);
+			myDictionary.Add("three", 3);
+
+			Console.WriteLine("Count: " + myDictionary.Count);
+			Console.WriteLine("Value for 'two': " + myDictionary["two"]);
+			Console.ReadLine();
+		}
+	}
+}
