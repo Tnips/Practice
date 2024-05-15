a05857b7ad4861a87ff869e1d5839ee6f6e669b9
Tnips <95481422+Tnips@users.noreply.github.com>
2024-05-15 08:49:15 +0300

    Complete task1

---
+++ lab21/task1/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+	public interface IStrategy
+	{
+		void Execute();
+	}
+
+	public class ConcreteStrategyA : IStrategy
+	{
+		public void Execute()
+		{
+			Console.WriteLine("Выполнена стратегия A");
+		}
+	}
+
+	public class ConcreteStrategyB : IStrategy
+	{
+		public void Execute()
+		{
+			Console.WriteLine("Выполнена стратегия B");
+		}
+	}
+
+	public class Context
+	{
+		private IStrategy _strategy;
+
+		public Context(IStrategy strategy)
+		{
+			this._strategy = strategy;
+		}
+
+		public void SetStrategy(IStrategy strategy)
+		{
+			this._strategy = strategy;
+		}
+
+		public void ExecuteStrategy()
+		{
+			_strategy.Execute();
+		}
+	}
+
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			var context = new Context(new ConcreteStrategyA());
+			context.ExecuteStrategy();
+
+			context.SetStrategy(new ConcreteStrategyB());
+			context.ExecuteStrategy();
+
+			Console.ReadLine();
+		}
+	}
+}
