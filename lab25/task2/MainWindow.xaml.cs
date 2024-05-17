740e601b51bc2d13e617f34aa2e218643044afcf
Tnips <95481422+Tnips@users.noreply.github.com>
2024-05-17 14:35:09 +0300

    Complete

---
+++ lab25/task2/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace task2
+{
+	/// <summary>
+	/// Логика взаимодействия для MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			double a = double.Parse(aTextBox.Text);
+			double b = double.Parse(bTextBox.Text);
+
+			canvas.Children.Clear();
+
+			Line xAxis = new Line { X1 = 0, Y1 = 400, X2 = 800, Y2 = 400, Stroke = Brushes.Black };
+			Line yAxis = new Line { X1 = 400, Y1 = 0, X2 = 400, Y2 = 800, Stroke = Brushes.Black };
+			canvas.Children.Add(xAxis);
+			canvas.Children.Add(yAxis);
+
+			Polyline polyline = new Polyline();
+			polyline.Stroke = Brushes.Black;
+
+			for (double x = -10; x <= 10; x += 0.1)
+			{
+				double y = a * x + b;
+				polyline.Points.Add(new Point(50 * x + 400, 400 - 50 * y));
+			}
+
+			canvas.Children.Add(polyline);
+		}
+	}
+}
---
+++ lab25/task4/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace task4
+{
+	/// <summary>
+	/// Логика взаимодействия для MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		private bool isMouseOverButton = false;
+		private Random random = new Random();
+
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void Button_MouseEnter(object sender, MouseEventArgs e)
+		{
+			if (!isMouseOverButton)
+			{
+				Button newButton = new Button();
+				newButton.Content = "Новая кнопка";
+				newButton.MouseEnter += Button_MouseEnter;
+				newButton.Click += Button_Click;
+				newButton.MouseLeave += Button_MouseLeave;
+
+				Canvas.SetLeft(newButton, random.Next(0, (int)canvas.ActualWidth - 100));
+				Canvas.SetTop(newButton, random.Next(0, (int)canvas.ActualHeight - 50));
+
+				canvas.Children.Add(newButton);
+				isMouseOverButton = true;
+			}
+		}
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			Button button = (Button)sender;
+			if (button.Parent is Panel parentPanel)
+			{
+				parentPanel.Children.Remove(button);
+			}
+		}
+
+		private void Button_MouseLeave(object sender, MouseEventArgs e)
+		{
+			isMouseOverButton = false;
+		}
+	}
+}
