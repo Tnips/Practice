e66a883fad864a1477ae053f0c88c43845116dc7
Tnips <95481422+Tnips@users.noreply.github.com>
2024-05-17 12:35:27 +0300

    Complete

---
+++ lab24/task3/CustomLabel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace task3
+{
+	internal class CustomLabel : Label
+	{
+		private Color defaultColor = Color.Black;
+		private Color hoverColor = Color.Red;
+		public CustomLabel()
+		{
+			this.MouseEnter += (sender, e) =>
+			{
+				this.ForeColor = hoverColor;
+			};
+
+			this.MouseLeave += (sender, e) =>
+			{
+				this.ForeColor = defaultColor;
+			};
+		}
+	}
+	
+}
